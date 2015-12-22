9c3f014c697159aa907e106cfe3b4cae42dfdf59
Jorge <lopez.jorge1@outlook.com>
2015-12-22 11:39:16 -0500

    Create character objects

---
+++ js/javascript.js
@@ -0,0 +1,60 @@
+var charmander = {
+	name: "Charmander",
+	health: 100,
+	lvl: 12,
+	effect: null,
+	moves: [{
+		name: "Ember",
+		type: "Attack",
+		power: 20,
+		accuracy: .80
+	}, 
+	{
+		name: "Scratch",
+		type: "Attack",
+		power: 10,
+		accuracy: .90
+	},
+	{
+		name: "Leer",
+		type: "Defense",
+		power: .20,
+		accuracy: 1.0
+	},
+	{
+		name: "Growl",
+		type: "Defense",
+		power: .65,
+		accuracy: .65
+	}]
+};
+var pikachu = {
+	name: "pikachu",
+	health: 100,
+	lvl: 9,
+	effect: null,
+	moves: [{
+		name: "Thunder Shock",
+		type: "Attack",
+		power: 10,
+		accuracy: .95
+	}, 
+	{
+		name: "Thunder Wave",
+		type: "Attack",
+		power: 25,
+		accuracy: .60
+	},
+	{
+		name: "Tail Whip",
+		type: "Defense",
+		power: .15,
+		accuracy: 1.0
+	},
+	{
+		name: "Growl",
+		type: "Defense",
+		power: .55,
+		accuracy: .55
+	}]
+};
